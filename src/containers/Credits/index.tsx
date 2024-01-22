f5860c08d8eb34a6560606db1e31ad804c1c955e
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-01-22 00:02:03 -0300

    feat: more info to credits

--- src/containers/Credits/index.tsx
+++ src/containers/Credits/index.tsx
@@ -7,7 +7,7 @@ const Credits = () => {
     return (
         <div className="container">
             <p>
-                Este jogo foi criado por <a href="https://victoroliveira.vercel.app/" target="_blank">Victor Freire Oliveira</a> (desenvolvedor web front-end), fã do canal <a href="https://www.youtube.com/@olabrasil" target="_blank">Olá Brasil</a>, de Alexis, no YouTube, no intuito de treinar habilidades de programação de aplicação web com React JS, e homenagear Alexis, Carol e Amélie. Toda a parte de design e ilustrações também foram criadas por Victor.
+                Este jogo foi criado por <a href="https://victoroliveira.vercel.app/" target="_blank">Victor Freire Oliveira</a> (desenvolvedor web front-end, designer multimídia e artista ilustrador), fã do canal <a href="https://www.youtube.com/@olabrasil" target="_blank">Olá Brasil</a>, de Alexis, no YouTube, no intuito de treinar habilidades de programação de aplicação web com React JS, e homenagear Alexis, Carol e Amélie. Toda a parte de design e ilustrações também foram criadas por Victor.
             </p>
             <S.logos>
                 <p>saiba mais:</p>
