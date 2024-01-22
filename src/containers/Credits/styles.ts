5599921d3913cd7336673a5f525d995c5449743f
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-01-21 23:51:21 -0300

    feat: add images on credits page

---
+++ src/containers/Credits/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const logos = styled.div`
+    margin-top: 40px;
+    font-size: 20px;
+
+    img {
+        width: 40px;
+        margin: 16px;
+        transition: all .2s ease;
+
+        &:hover {
+            transform: translateY(-6px);
+        }
+    }
+`
